78256f1aa445da99a223fe28f9a03adcd378f56f
Cat Book <75529688+CatBook2010@users.noreply.github.com>
2021-12-17 10:16:31 +0000

    create show_infromation method

--- human.py
+++ human.py
@@ -2,3 +2,6 @@ class Human:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+    
+    def show_information(self):
+        print(f'{self.name} is {self.age} y.o.')
